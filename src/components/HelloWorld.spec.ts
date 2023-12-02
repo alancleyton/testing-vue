78b661454c3e62f4baa8a6cfb63df3cdcc75353a
Alan Cleyton <alancleyton67@gmail.com>
2023-12-02 20:03:33 -0300

    test: add HelloWorld component whitebox and blackbox specs 🚨

--- src/components/HelloWorld.spec.ts
+++ src/components/HelloWorld.spec.ts
@@ -12,4 +12,27 @@ describe('HelloWorld.vue', () => {
 
     expect(instance.html()).toContain('Hello World!');
   });
+
+  // whitebox test
+  it('should increment count when increase method is called', () => {
+    const instance = mount(HelloWorld);
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    instance.vm.increment();
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    expect(instance.vm.count).toBe(1);
+  });
+
+  // blackbox test
+  it('should increment count when button clicked', async () => {
+    const instance = mount(HelloWorld);
+
+    const button = instance.find('button');
+    await button.trigger('click');
+
+    expect(button.text()).toBe('count is 1');
+  });
 });
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,13 +4,15 @@ import { ref } from 'vue';
 defineProps<{ msg: string }>();
 
 const count = ref(0);
+
+const increment = () => count.value++;
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="increment">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
