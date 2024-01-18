86f9fdf0ea927827c5704578e76dcfc88b000a0a
Alan Cleyton <alancleyton67@gmail.com>
2024-01-18 16:07:28 -0300

    🚨 test: add default options to HelloWorld spec

--- src/components/HelloWorld.spec.ts
+++ src/components/HelloWorld.spec.ts
@@ -2,20 +2,22 @@ import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
+const defaultOptions = {
+  props: {
+    msg: 'Hello World!',
+  },
+};
+
 describe('HelloWorld.vue', () => {
   it('should render with msg prop', () => {
-    const instance = mount(HelloWorld, {
-      props: {
-        msg: 'Hello World!',
-      },
-    });
+    const instance = mount(HelloWorld, defaultOptions);
 
     expect(instance.html()).toContain('Hello World!');
   });
 
   // whitebox test
   it('should increment count when increase method is called', () => {
-    const instance = mount(HelloWorld);
+    const instance = mount(HelloWorld, defaultOptions);
 
     // eslint-disable-next-line
     // @ts-ignore
@@ -28,7 +30,7 @@ describe('HelloWorld.vue', () => {
 
   // blackbox test
   it('should increment count when button clicked', async () => {
-    const instance = mount(HelloWorld);
+    const instance = mount(HelloWorld, defaultOptions);
 
     const button = instance.find('button');
     await button.trigger('click');
