9231ff70dd342f52791408946dad87bf6f8e481a
Alan Cleyton <alancleyton67@gmail.com>
2024-01-25 11:49:23 -0300

    ✨ feat: add project router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<template>
+  <div>
+    <h2>Home view</h2>
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
