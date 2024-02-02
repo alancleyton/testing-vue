3b5fbfbff85a3398d4fc3c7511be4f4e324c6708
Alan Cleyton <alancleyton67@gmail.com>
2024-02-02 17:16:36 -0300

    🎉 feat: list todos with useTodos composable

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 
+import { useTodos } from '@/composables/useTodos';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
+
+const { getTodos, todos, isLoading } = useTodos();
+
 onMounted(() => {
-  fetch('https://jsonplaceholder.typicode.com/todos');
+  getTodos();
 });
 </script>
 
 <template>
-  <ul class="flex flex-col gap-3 list-none m-0 p-0">
-    <li class="block">
-      <div class="relative bg-green-50 shadow-md bg-clip-border rounded-xl p-4">
-        <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-          delectus aut autem
-        </p>
-      </div>
-    </li>
-
-    <li class="block">
-      <div class="relative bg-red-50 shadow-md bg-clip-border rounded-xl p-4">
-        <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-          quis ut nam facilis et officia qui
-        </p>
-      </div>
+  <LoadingSpinner v-if="isLoading" />
+  <ul v-else class="flex flex-col gap-3 list-none m-0 p-0">
+    <li
+      class="relative shadow-md bg-clip-border rounded-xl p-4"
+      :class="{ 'bg-green-50': todo.completed, 'bg-red-50': !todo.completed }"
+      v-for="todo in todos"
+      :key="todo.id + todo.userId"
+    >
+      <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+        {{ todo.title }}
+      </p>
     </li>
   </ul>
 </template>
